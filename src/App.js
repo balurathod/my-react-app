263644a2c4b9a3e34db517e22703582a1e4be838
Balu Rathod <rathodbalu2@gmail.com>
2023-10-01 09:15:42 +0530

    added Navbar and TextForm

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import './App.css';
-import MyButton from './Components/Button/MyButton';
-//import ShoppingList from './Components/List/List';
+import "./App.css";
+// import MyButton from "./Components/Button/MyButton";
+// import ShoppingList from './Components/List/List';
 // import TicTacToe from './Components/TicTacToe/ticTacToe';
-
 // import CreateFolderFiles from './Components/CreateFolderFile/CreateFolderFiles';
+import Navbar from "./Components/common/Navbar/Navbar";
+import TextForm from "./Components/TextForm/TextForm";
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to my REACT app</h1>
-      <MyButton />
-      <MyButton />
+      {/* <MyButton />
+      <MyButton /> */}
       {/* <ShoppingList /> */}
       {/* <TicTacToe /> */}
       {/* <CreateFolderFiles /> */}
+      {/* <Navbar title="TextUtils" aboutText="About TextUtils"/> */}
+      <Navbar /> 
+<TextForm heading="Enter the text to analyze below"/>
     </div>
   );
 }
